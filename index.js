0e7d90b69244733b26a41da2cfa003d6e3fc9e2b
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-03-27 10:36:59 -0700

    commit tree efc6af975c962116a8e839e00f68016bb2d606f9

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(activity= "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+  
+  const Calculator = {
+    add: function(x,y) {
+      return x + y;
+    },
+    subtract: function(x,y) {
+      return x - y
+    },
+    multiply: function(x,y) {
+      return x * y
+    },
+    divide: function(x,y) {
+      return x / y;
+    }
+  }
+
+  let actionApplyer = function(start, ray) {
+    let x = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      x = ray[i](x)
+    }
+  
+    return x
+  }
